27dc38b1d4b6712d2eab6bb1079f59e109ab474c
igorburak <biv992@gmail.com>
2022-08-28 14:26:26 +0200

    add Dashboard component

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface IProps {
+
+}
+
+const Dashboard = (props: IProps) => {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    )
+}
+
+export default Dashboard
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 import {createTheme, CssBaseline, ThemeProvider} from '@mui/material'
 import {red} from '@mui/material/colors'
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
-);
+)
 
 const theme = createTheme({
     palette: {
@@ -22,7 +22,7 @@ const theme = createTheme({
             main: red.A400,
         },
     },
-});
+})
 
 root.render(
     <React.StrictMode>
@@ -31,9 +31,9 @@ root.render(
             <App/>
         </ThemeProvider>
     </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
